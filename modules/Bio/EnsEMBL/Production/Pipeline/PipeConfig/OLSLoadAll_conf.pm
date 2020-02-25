d17457af1ab456c34220fc7e2e4903e77f1c1a83
Luca Da Rin Fioretto <ldrf@ebi.ac.uk>
2020-02-25 15:59:39 +0000

    Add CompareOntologyTerm_Datacheck to OLSLoadAll_conf

--- modules/Bio/EnsEMBL/Production/Pipeline/PipeConfig/OLSLoadAll_conf.pm
+++ modules/Bio/EnsEMBL/Production/Pipeline/PipeConfig/OLSLoadAll_conf.pm
@@ -21,23 +21,27 @@ limitations under the License.
 
 =head1 DESCRIPTION
 
-    Hive pipeline to load all required ontologies into a dedicated mysql database
-    The pipeline will create a database named from current expected release number, load expected ontologies from OLS,
-    Check and compute terms closure.
+    Hive pipeline to load all required ontologies into a dedicated mysql
+    database The pipeline will create a database named from current expected
+    release number, load expected ontologies from OLS, Check and compute terms
+    closure.
 
 =head1 LICENSE
 
-    Copyright [2016-2020] EMBL-European Bioinformatics Institute
-    Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
-         http://www.apache.org/licenses/LICENSE-2.0
-    Unless required by applicable law or agreed to in writing, software distributed under the License
-    is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and limitations under the License.
+    Copyright [2016-2020] EMBL-European Bioinformatics Institute Licensed
+    under the Apache License, Version 2.0 (the "License"); you may not use this
+    file except in compliance with the License.  You may obtain a copy of the
+    License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by
+    applicable law or agreed to in writing, software distributed under the
+    License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+    OF ANY KIND, either express or implied.  See the License for the specific
+    language governing permissions and limitations under the License.
 
 =head1 CONTACT
 
-    Please subscribe to the Hive mailing list:  http://listserver.ebi.ac.uk/mailman/listinfo/ehive-users  to discuss Hive-related questions or to be notified of our updates
+    Please subscribe to the Hive mailing list:
+    http://listserver.ebi.ac.uk/mailman/listinfo/ehive-users to discuss
+    Hive-related questions or to be notified of our updates
 
 =cut
 
@@ -53,8 +57,9 @@ sub default_options {
     return {
         %{$self->SUPER::default_options},
         'wipe_all'   => 1,
-        'ontologies'    => ['GO', 'SO', 'PATO', 'HP', 'VT', 'EFO', 'PO', 'EO', 'TO', 'CHEBI', 'PR', 'FYPO', 'PECO', 'BFO',
-                          'BTO', 'CL', 'CMO', 'ECO', 'MP', 'OGMS', 'UO', 'MONDO', 'PHI']
+        'ontologies'    => ['GO', 'SO', 'PATO', 'HP', 'VT', 'EFO', 'PO', 'EO',
+                            'TO', 'CHEBI', 'PR', 'FYPO', 'PECO', 'BFO', 'BTO',
+                            'CL', 'CMO', 'ECO', 'MP', 'OGMS', 'UO', 'MONDO', 'PHI']
     }
 }
 
--- modules/Bio/EnsEMBL/Production/Pipeline/PipeConfig/OLSLoad_conf.pm
+++ modules/Bio/EnsEMBL/Production/Pipeline/PipeConfig/OLSLoad_conf.pm
@@ -21,22 +21,26 @@ limitations under the License.
 
 =head1 DESCRIPTION
 
-Hive pipeline to load all required ontologies into a dedicated mysql database
-The pipeline will create a database named from current expected release number, load expected ontologies from OLS,
-Check and compute terms closure.
+    Hive pipeline to load all required ontologies into a dedicated mysql
+    database The pipeline will create a database named from current expected
+    release number, load expected ontologies from OLS, Check and compute terms
+    closure.
 
 =head1 LICENSE
-    Copyright [2016-2020] EMBL-European Bioinformatics Institute
-    Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
-         http://www.apache.org/licenses/LICENSE-2.0
-    Unless required by applicable law or agreed to in writing, software distributed under the License
-    is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and limitations under the License.
+    Copyright [2016-2020] EMBL-European Bioinformatics Institute Licensed
+    under the Apache License, Version 2.0 (the "License"); you may not use this
+    file except in compliance with the License.  You may obtain a copy of the
+    License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by
+    applicable law or agreed to in writing, software distributed under the
+    License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+    OF ANY KIND, either express or implied.  See the License for the specific
+    language governing permissions and limitations under the License.
 
 =head1 CONTACT
 
-    Please subscribe to the Hive mailing list:  http://listserver.ebi.ac.uk/mailman/listinfo/ehive-users  to discuss Hive-related questions or to be notified of our updates
+    Please subscribe to the Hive mailing list:
+    http://listserver.ebi.ac.uk/mailman/listinfo/ehive-users to discuss
+    Hive-related questions or to be notified of our updates
 
 =cut
 
@@ -69,7 +73,8 @@ sub default_options {
       'pipeline_name' => 'ols_ontology_' . $self->o('ens_version'),
       'db_url'        => $self->o('db_host') . $self->o('db_name'),
       'ontologies'    => [],
-      'skip_phi'      => 0
+      'skip_phi'      => 0,
+      'old_server'    => 'mysql://ensro@mysql-ens-mirror-1:4240'
   }
 }
 
@@ -257,7 +262,18 @@ sub pipeline_analyses {
               mart => $self->o('mart_db_name'),
               srv  => $self->o('srv')
           }
-      }
+      },
+      {
+      -logic_name        => 'CompareOntologyTerm_Datacheck',
+      -module            => 'Bio::EnsEMBL::DataCheck::Pipeline::RunDataChecks',
+      -parameters        => {
+                              datacheck_names => ['CompareOntologyTerm'],
+                              history_file    => $self->o('history_file'),
+                              old_server_uri  => $self->o('old_server'),
+                              registry_file   => $self->o('reg_file'),
+                              failures_fatal  => 1
+                            }
+    }
   ];
 }
 
